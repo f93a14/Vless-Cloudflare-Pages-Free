0f50837a7a78b8df73f8ecbad82c5a60320603b6
f93a14 <161033942+f93a14@users.noreply.github.com>
2024-02-23 16:28:39 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 // @armanibash
 import { connect } from 'cloudflare:sockets';
 
-let userID = '73ed240d-57b6-4fda-8458-a9b40cf5653d';
+let userID = 'd7ea844f-d0ff-4108-8a94-21fa7e6b2125';
 
 const proxyIPs = ["[2001:67c:2b0:db32:0:1:681a:404]"]; // ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
